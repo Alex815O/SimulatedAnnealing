07917c63560897b9f7b6971dc65b00c6d64e7244
Alex Ofner <e12119038@student.tuwien.ac.at>
2026-06-01 22:33:52 +0200

    extract swap machine neigbor

--- neighbourhood.py
+++ neighbourhood.py
@@ -18,28 +18,11 @@ def generate_neighbour(solution, input_data):
 
         if move_type == "swap_order":
             neighbour = swap_order_on_same_machine(neighbour, input_data)
-            if neighbour is None:
-                continue
-
         elif move_type == "change_machine":
-            i = rand.randrange(jobs_nr)
-            job_id = neighbour[i]["JobId"]
+            neighbour = swap_machine(neighbour, input_data, jobs_nr)
 
-            ctx_job = next(job for job in input_data["Jobs"] if job["Id"] == job_id)
-            eligible_machines = ctx_job["EligibleMachineIds"]
-
-            # If there is only one eligible machine, this move cannot change anything.
-            if len(eligible_machines) <= 1:
-                continue
-
-            current_machine = neighbour[i]["MachineId"]
-            possible_machines = [m for m in eligible_machines if m != current_machine]
-
-            if not possible_machines:
-                continue
-
-            neighbour[i]["MachineId"] = rand.choice(possible_machines)
-
+        if neighbour is None:
+            continue
         rebuilt = rebuild_schedule(neighbour, input_data)
 
         if rebuilt is not None and constraints.validate(rebuilt, input_data):
@@ -89,6 +72,27 @@ def switch_jobs(solution, job_to_move, job_to_switch):
         solution[job_to_move]["DueTime"],
         solution[job_to_switch]["DueTime"],
     )
+    return solution
+
+
+def swap_machine(solution, context, jobs_nr):
+    i = rand.randrange(jobs_nr)
+    job_id = solution[i]["JobId"]
+
+    ctx_job = next(job for job in context["Jobs"] if job["Id"] == job_id)
+    eligible_machines = ctx_job["EligibleMachineIds"]
+
+    # If there is only one eligible machine, this move cannot change anything.
+    if len(eligible_machines) <= 1:
+        return None
+
+    current_machine = solution[i]["MachineId"]
+    possible_machines = [m for m in eligible_machines if m != current_machine]
+
+    if not possible_machines:
+        return None
+
+    solution[i]["MachineId"] = rand.choice(possible_machines)
     return solution
 
 
